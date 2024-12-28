76d4fee5ab37f081501e897386add935fef57655
MpG <127748209+MmppGG@users.noreply.github.com>
2024-12-28 16:20:42 +0000

    first commit

---
+++ src/main/java/com/example/Evote/EvoteApplication.java
@@ -0,0 +1,13 @@
+package com.example.Evote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Evote/EvoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Evote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
